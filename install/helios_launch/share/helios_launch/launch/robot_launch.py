1d33fc8f3997aa03d2d497e2806018716fb074d1
Jaden Howard <jaseanhow@gmail.com>
2025-05-23 07:34:30 -0400

    gazebo

---
+++ install/helios_launch/share/helios_launch/launch/robot_launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    helios_sim_share = FindPackageShare('helios_sim')
+    helios_control_share = FindPackageShare('helios_control')
+
+    world_file = PathJoinSubstitution([helios_sim_share, 'worlds', 'warehouse.sdf'])
+    bridge_config = PathJoinSubstitution([helios_control_share, 'config', 'rosbridge_topics.yaml'])
+
+    gazebo = ExecuteProcess(
+        cmd=['ign', 'gazebo', '-r', world_file],
+        output='screen'
+    )
+
+   
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{'config_file': bridge_config}],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo,
+        bridge
+    ])
